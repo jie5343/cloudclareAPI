ece83c58084e6f71786fdea55899656d85b196c1
jie5343 <jie5343@126.com>
2020-03-17 16:18:14 +0800

    Update 1.php

--- 1.php
+++ 1.php
@@ -5,7 +5,7 @@ $cf=new CFAPI;
 $cf->apikey="xtMgnqYnvEtBAfVhunfEMjad7cRKAGRu";
 $cf->email="test@test.com";
 $cf->zoneid="ehv34HCJoZKw9RJoTjqhupBqLKw3p7tV";
-$cf->updateDNS("www.91yun.org","1.1.1.1");		
+$cf->updateDNS("www.yun.org","1.1.1.1");		
 
 
 ?>
